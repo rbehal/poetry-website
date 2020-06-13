70c5acea1f4feae0a81b65ee436f1269d9392c93
rbehal <rahulbehal01@hotmail.com>
2020-06-13 15:13:22 -0400

    Configured backend

--- backend/Backend.py
+++ backend/Backend.py
@@ -1,20 +1,30 @@
 # Using flask to make an api 
 # import necessary libraries and functions 
 import flask
-import requests
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, redirect
 from flask_restful import Api, Resource, reqparse
 from flask_cors import CORS, cross_origin
+import os
 
 # creating a Flask app 
 app = Flask(__name__) 
 
 
 @app.route("/", methods = ['GET']) 
-def home_view(): 
-        return "<h1>Poem website backend is deployed :)</h1>"
+def home_view():
+	return "<h1>Poem website backend is deployed :)</h1>"
 
+@app.route("/upload", methods = ['GET','POST']) 
+def upload_image():
+	if request.method == "POST":
+		if request.files:
+			poems = request.files["poems"]
+			print(poems)
+			print(os.getcwd())
+			poems.save(os.getcwd())
+			return redirect(request.referrer)
+	return 'NOT OK'
 
 # driver function - must be at the end
 if __name__ == '__main__': 
-    app.run() 
+	app.run()
--- frontend/src/Upload.vue
+++ frontend/src/Upload.vue
@@ -2,7 +2,23 @@
     <div class="row">
         <div class="col" style="text-align: center">
             <h1>Upload Page</h1>
-            <b-form-file v-model="file2" class="py-2" plain></b-form-file>
+            <form action="http://127.0.0.1:6001/upload" method="POST" enctype="multipart/form-data">
+                <b-form-file 
+                    v-model="file" 
+                    class="py-2" 
+                    accept="application/pdf"
+                    name="poems"
+                    plain
+                >
+                </b-form-file>
+                <b-button 
+                    type="submit" 
+                    variant="info"
+                    class="upload-button"
+                >
+                    Upload
+                </b-button>
+            </form>
         </div>
     </div>
 </template>
@@ -14,3 +30,12 @@ export default {
 
 }
 </script>
+
+<style> 
+.upload-button {
+    margin-top: 1%;
+    margin-bottom: 1%;
+    height: 50px;
+    width: 150px;
+}
+</style>
