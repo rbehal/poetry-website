d3878b1a153798883f29a480563ae6b38eced9f0
rbehal <rahulbehal01@hotmail.com>
2020-06-13 21:37:57 -0400

    Added image upload and retrieve functionality

--- backend/Backend.py
+++ backend/Backend.py
@@ -4,9 +4,14 @@ import flask
 from flask import Flask, jsonify, request, redirect
 from flask_restful import Api, Resource, reqparse
 from flask_cors import CORS, cross_origin
+
 import os
 import cloudconvert
 import zipfile
+
+import cloudinary.uploader
+import cloudinary.api
+import json
 
 # PDF Processing #
 
@@ -24,26 +29,49 @@ def downloadImages():
 	process.download("raw_images/")
 
 def createRawImages():
+	images_path = "raw_images/"
+
 	# Extracting zip file
-	with zipfile.ZipFile("raw_images/poems.zip", 'r') as zip_ref:
-		zip_ref.extractall("raw_images/")
+	with zipfile.ZipFile(images_path + "poems.zip", 'r') as zip_ref:
+		zip_ref.extractall(images_path)
 
 	# Deleting zip file
-	os.remove("raw_images/poems.zip")
+	os.remove(images_path + "poems.zip")
 	os.remove("poems.pdf")
 
 	# Renaming images
-	for filename in os.listdir("raw_images/"):
+	num_pages = 0
+	for filename in os.listdir(images_path):
 		if filename.endswith(".png"):
-			image_path = "raw_images/"
 			if (filename[-7:-4].isdigit()):
-				os.rename(image_path + filename, image_path + filename[-7:-4] + ".png")
+				os.rename(images_path + filename, images_path + filename[-7:-4] + ".png")
+				num_pages += 1
 			elif (filename[-6:-4].isdigit()):
-				os.rename(image_path + filename, image_path + filename[-6:-4] + ".png")
+				os.rename(images_path + filename, images_path + filename[-6:-4] + ".png")
+				num_pages += 1
 			elif (filename[-5].isdigit()):
-				os.rename(image_path + filename, image_path + filename[-5] + ".png")    
+				os.rename(images_path + filename, images_path + filename[-5] + ".png")    
+				num_pages += 1
 		else:
 			continue
+	uploadImages(num_pages)
+
+def uploadImages(num_pages):
+	# Delete old images
+	cloudinary.api.delete_resources_by_prefix("raw_images")
+
+	image_links = []
+	for i in range(1, num_pages + 1):
+		upload_ref = cloudinary.uploader.upload("raw_images/" + str(i) + ".png", use_filename=True, folder="raw_images")
+		image_links.append(upload_ref['secure_url'])
+
+	with open('poems.json', 'w') as f:
+    	json.dump(image_links, f)
+
+def retrieveImages():
+	with open('poems.json', 'r') as f:
+		image_links = json.load(f)
+	return jsonify(image_links)
 
 
 # Creating a Flask app #
@@ -64,6 +92,11 @@ def upload_image():
 			createRawImages()
 			return redirect(request.referrer)
 	return 'NOT OK'
+
+@app.route("/retrieve", methods = ['GET'])
+def retrieve_images():
+	if request.method == "GET":
+		return retrieveImages()
 
 # Driver function - must be at the end
 if __name__ == '__main__': 
